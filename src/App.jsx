dbed570d5ea95a8cf66b8edf52ff8a04959c1761
Mattewska <mateootm@gmail.com>
2025-05-10 21:40:44 -0500

    loop infinito resuelto

--- src/App.jsx
+++ src/App.jsx
@@ -14,28 +14,28 @@ export function Square({ value, squareFunction }) {
 
 export default function Board() {
   const [squareValue, setSquareValue] = useState(Array(9).fill(null));
-  function handleClick() {
+  function handleClick(index) {
     const square = squareValue.slice();
-    square[0] = "x";
+    square[index] = "x";
     setSquareValue(square);
     console.log(squareValue);
   }
   return (
     <>
       <div className="board-row">
-        <Square value={squareValue[0]} squareFunction={handleClick} />
-        <Square value={squareValue[1]} squareFunction={handleClick} />
-        <Square value={squareValue[2]} squareFunction={handleClick} />
+        <Square value={squareValue[0]} squareFunction={() => handleClick(0)} />
+        <Square value={squareValue[1]} squareFunction={() => handleClick(1)} />
+        <Square value={squareValue[2]} squareFunction={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squareValue[3]} squareFunction={handleClick} />
-        <Square value={squareValue[4]} squareFunction={handleClick} />
-        <Square value={squareValue[5]} squareFunction={handleClick} />
+        <Square value={squareValue[3]} squareFunction={() => handleClick(3)} />
+        <Square value={squareValue[4]} squareFunction={() => handleClick(4)} />
+        <Square value={squareValue[5]} squareFunction={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squareValue[6]} squareFunction={handleClick} />
-        <Square value={squareValue[7]} squareFunction={handleClick} />
-        <Square value={squareValue[8]} squareFunction={handleClick} />
+        <Square value={squareValue[6]} squareFunction={() => handleClick(6)} />
+        <Square value={squareValue[7]} squareFunction={() => handleClick(7)} />
+        <Square value={squareValue[8]} squareFunction={() => handleClick(8)} />
       </div>
     </>
   );
